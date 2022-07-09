8a26661b1ce4c78ac8efe610abe5b6de0513e586
Jorge Trad <jetradl@gmail.com>
2022-07-09 10:22:21 -0500

    Reasign y redeclare

---
+++ source/scope/reasigning&redeclaration.js
@@ -0,0 +1,36 @@
+//VAR
+var firstName; //declaration -> undefined
+firstName = 'Oscar'; //Asign
+console.log(firstName);
+
+var lastName = 'David'; //Declaration & asign
+lastName = 'Ana'; //re asign
+console.log(lastName);
+
+var secondName = 'David'; //Declaration & asign
+var secondName = 'Ana'; //re asign
+console.log(secondName);
+
+//LET
+let fruit = 'Apple'; //Declaration & asign
+fruit = 'Kiwi'; ////re asign
+console.log(fruit);
+
+let fruit = 'Banana';
+console.log(fruit);
+
+//CONST
+const animal = 'dog'; //Declaration & asign
+animal = 'cat'; //re asign
+console.log(animal);
+
+const animal = 'Snake';
+console.log(animal);
+
+const vehicles = [];
+vehicles.push('🚗');
+vehicles.push('🚕');
+console.table(vehicles);
+
+vehicles.pop();
+console.table(vehicles);
